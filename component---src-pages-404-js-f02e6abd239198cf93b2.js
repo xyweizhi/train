b804b20348a82ba3d8c0980a534644c58c273209
caizhuanxi <caizhuanxi@wufeng-network.com>
2021-11-01 15:55:50 +0800

    Updates

---
+++ component---src-pages-404-js-f02e6abd239198cf93b2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(t,u,e){e.r(u);e(7294);var s=e(9613),n=e(3751),a=e(7918);u.default=function(){return(0,a.tZ)(s.Z,null,(0,a.tZ)(n.Z,{title:"404: Not found"}),(0,a.tZ)("h1",null,"404: Not Found"),(0,a.tZ)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f02e6abd239198cf93b2.js.map
